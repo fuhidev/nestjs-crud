2b7b78272f34a7f8137c1fe7ba3ab6cd14c8e6f3
PhuongHieu <fuhi.dev@gmail.com>
2023-11-01 17:31:02 +0700

    TypeoOrmModule

--- test/src/app.module.ts
+++ test/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [AppController],
   providers: [AppService],
 })
